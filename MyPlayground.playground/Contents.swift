b8347454eb2d8ec1884c285d8027114376177618
stillgood96 <a787574@naver.com>
2023-08-06 23:32:22 +0900

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var greeting = "Hello, swift"
+
+print(greeting)
+print("Hello I'm Sunwoo")
+print(greeting)
+
+UIImage(systemName: "star")
+
+var sum = 0
+for i in 1...10 {
+    sum += i
+}
+
+print(sum);
+
+
+
+
